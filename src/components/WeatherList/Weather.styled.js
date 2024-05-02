dcd3adf17803a8b73bf57e1516b33f8cb220565e
ANURUDDH SINGH <anurudhsingh069@gmail.com>
2024-05-02 15:29:50 +0530

    update

--- src/components/WeatherList/Weather.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 0;
-`;
